ecd7e505a1f861b05388cc2007ac55742fa08f61
lotuc <lotu.c@outlook.com>
2023-08-23 12:12:15 +0800

    remove mulog dependency

--- src/safely/core.clj
+++ src/safely/core.clj
@@ -1,8 +1,7 @@
 (ns safely.core
   (:require [clojure.tools.logging :as log]
             [defun.core :refer [defun]]
-            [safely.circuit-breaker :refer [execute-with-circuit-breaker]]
-            [com.brunobonacci.mulog :as u]))
+            [safely.circuit-breaker :refer [execute-with-circuit-breaker]]))
 
 
 
@@ -103,12 +102,10 @@
 (defmacro mutrace
   "utility macro for tracing"
   {:no-doc true}
-  [status event-name config-map & body]
+  [status _event-name _config-map & body]
   `(if (= :disabled ~status)
      (do ~@body)
-     (u/trace ~event-name
-       ~config-map
-       ~@body)))
+     (do (log/debugf "mulog tracing unsupported") ~@body)))
 
 
 
@@ -296,9 +293,9 @@
 
 (defn- make-attempt-with-circuit-breaker
   [{:keys [message log-ns log-errors log-level log-stacktrace call-site] :as opts} f]
-  (let [ ;; transfer local-context to circuit-breaker thread
-        ctx (u/local-context)
-        f   (fn [] (u/with-context ctx (f)))
+  (let [;; transfer local-context to circuit-breaker thread
+        ;; ctx (u/local-context)
+        ;; f   (fn [] (u/with-context ctx (f)))
         ;; enquque call
         [value error :as result] (->> (execute-with-circuit-breaker f opts)
                                    (normalize-failure opts))]
